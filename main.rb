ecfd6da04c43ccb0b834ad62531e032aaf541df8
Ginny Hendry <contact@ghendry.com>
2011-03-25 19:51:32 -0500

    active record notification for sql

--- main.rb
+++ main.rb
@@ -9,15 +9,32 @@ module PeopleList
 
   class App < Sinatra::Base
     include PeopleConnect
-    ActiveRecord::Base.logger = Logger.new("ar.log")
 
     dir = File.dirname(File.expand_path(__FILE__))
     set :public, "#{dir}/public"
     set :views, "#{dir}/views"
 
+    ActiveRecord::Base.logger = Logger.new("ar.log")
+    file_name = "#{dir}/logs/sql.log"
+    ActiveSupport::Notifications.subscribe /^sql\./ do |*args| 
+      self.log_query(file_name, args)
+    end
+
     def initialize(*args)
       super
       db_connect
+    end
+
+    def self.log_query(file_name,args)
+      File.open(file_name,'a') do |f|
+        f.print "#{args[0]}|"
+        f.print "#{args[1].strftime('%Y-%m-%d %H:%M:%S')}|"
+        f.print "#{args[2].strftime('%Y-%m-%d %H:%M:%S')}|"
+        f.print "#{args[2]-args[1]}|"
+        f.print "#{args[4][:name]}|"
+        f.print "#{args[4][:sql].strip.gsub(/\n/,'').squeeze(' ')}"
+        f.puts
+      end
     end
 
     helpers AppHelpers
